415ba1d3638094dcca9a905fcf134174e2f50cbe
Gustavo Yokoyama Ribeiro <gyr@keemail.me>
2025-12-02 13:06:45 -0300

    Add json file parser for bugowner
    Signed-off-by: Gustavo Yokoyama Ribeiro <gyribeiro@suse.com>

--- pkg/app/bugowner.go
+++ pkg/app/bugowner.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/gyr/relx-go/pkg/config"
 	"github.com/gyr/relx-go/pkg/gitutils"
@@ -9,9 +12,6 @@ import (
 
 // HandleBugownerByPackage fetches and displays the bug owners for a given package.
 func HandleBugownerByPackage(cfg *config.Config, pkg string) {
-	// TODO: Implement the logic to fetch bug owners for the package.
-	// This will likely involve interacting with a bug tracking system API.
-	// For now, we'll just print a placeholder message.
 	cfg.Logger.Infof("Handling bug owner request for package %s", pkg)
 	fmt.Printf("Fetching bug owners for package: %s\n", pkg)
 
@@ -21,6 +21,16 @@ func HandleBugownerByPackage(cfg *config.Config, pkg string) {
 		cfg.Logger.Fatalf("Error cloning/updating repository: %v", err)
 	}
 	cfg.Logger.Infof("Repository available at: %s", localPath)
+
+	maintainers, err := loadMaintainershipData(localPath)
+	if err != nil {
+		cfg.Logger.Fatalf("Error loading maintainership data: %v", err)
+	}
+
+	cfg.Logger.Debugf("Loaded maintainership data: %+v", maintainers)
+
+	// TODO: Use the maintainers map to find bug owners for the package.
+	// For now, we'll just print a placeholder message.
 }
 
 // HandlePackagesByMaintainer lists the packages maintained by a given user.
@@ -30,4 +40,22 @@ func HandlePackagesByMaintainer(cfg *config.Config, maintainer string) {
 	// For now, we'll just print a placeholder message.
 	fmt.Printf("Listing packages for maintainer: %s\n", maintainer)
 	cfg.Logger.Infof("Handling packages by maintainer request for %s", maintainer)
+}
+
+// loadMaintainershipData reads and unmarshals the _maintainership.json file.
+func loadMaintainershipData(localPath string) (map[string][]string, error) {
+	maintainershipFilename := "_maintainership.json"
+	maintainershipFile := filepath.Join(localPath, maintainershipFilename)
+
+	data, err := os.ReadFile(maintainershipFile)
+	if err != nil {
+		return nil, fmt.Errorf("error reading maintainership file %s: %w", maintainershipFile, err)
+	}
+
+	var maintainers map[string][]string
+	if err := json.Unmarshal(data, &maintainers); err != nil {
+		return nil, fmt.Errorf("error unmarshaling maintainership JSON from %s: %w", maintainershipFile, err)
+	}
+
+	return maintainers, nil
 }
